71c673df7172264685f026c371d74b877d2ab6c2
Shahmeer <ashahmeer73@gmail.com>
2025-02-02 21:08:32 +0500

    structure Ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Syed Shahmeer Ali",
+    author_email="ashahmeer73@gmail.com",
+    packages=find_packages()
+)
